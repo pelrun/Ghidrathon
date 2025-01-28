96daf5648a7779c0b6c4e973342e8fcb5dc865fd
James Churchill <pelrun@gmail.com>
2025-01-29 00:40:24 +1100

    Use @runtime to select ghidrathon instead of hijacking .py

--- src/main/java/ghidrathon/GhidrathonScriptProvider.java
+++ src/main/java/ghidrathon/GhidrathonScriptProvider.java
@@ -13,12 +13,12 @@ package ghidrathon;
 import generic.jar.ResourceFile;
 import ghidra.app.script.GhidraScript;
 import ghidra.app.script.GhidraScriptLoadException;
-import ghidra.app.script.GhidraScriptProvider;
+import ghidra.app.script.AbstractPythonScriptProvider;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class GhidrathonScriptProvider extends GhidraScriptProvider {
+public class GhidrathonScriptProvider extends AbstractPythonScriptProvider {
 
   @Override
   public String getDescription() {
@@ -30,6 +30,12 @@ public class GhidrathonScriptProvider extends GhidraScriptProvider {
   public String getExtension() {
 
     return ".py";
+  }
+
+  @Override
+  public String getRuntimeEnvironmentName() {
+
+    return "Ghidrathon";
   }
 
   @Override
@@ -64,25 +70,4 @@ public class GhidrathonScriptProvider extends GhidraScriptProvider {
     return "#";
   }
 
-  /**
-   * Commandeer the .py script extension
-   *
-   * <p>Ghidra loads script providers in order determined by Collections.sort; Ghidra then selects
-   * the first script provider that accepts the file extension of the script to be executed see
-   * https://github.com/NationalSecurityAgency/ghidra/blob/8b2ea61e27c07c48dc21eff9095905f739208703/Ghidra/Features/Base/src/main/java/ghidra/app/script/GhidraScriptUtil.java#L274-L281
-   *
-   * <p>Collections.sort invokes GhidraScriptProvider.compareTo so we can override compareTo and
-   * check if the script provider we are being compared to uses the .py extension; if true, we
-   * simply return -1 to be ordered higher in the list of script providers used by Ghidra
-   */
-  @Override
-  public int compareTo(GhidraScriptProvider that) {
-
-    if (that.getExtension().equals(".py")) {
-      // return -1 so our script provider is preferred
-      return -1;
-    }
-
-    return super.compareTo(that);
-  }
 }
